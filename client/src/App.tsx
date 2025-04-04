7273b953ce871d794253e286ba1564472c1749b7
Yasunori MAHATA <mahata777@gmail.com>
2025-04-04 12:54:08 +0900

    ✨ enhance App component to send user-input messages via WebSocket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import useWebSocket from 'react-use-websocket';
 
 const WS_URL = 'ws://localhost:8080';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const { sendJsonMessage, readyState } = useWebSocket(WS_URL, {
+  const [text, setText] = useState('');
+
+  const { sendJsonMessage } = useWebSocket(WS_URL, {
     onOpen: () => {
       console.log('WebSocket connection established.')
     },
   });
 
-  useEffect(() => {
-    if (readyState === WebSocket.OPEN) {
-      sendJsonMessage({ message: 'Hello, server!' });
-    }
-  }, [sendJsonMessage, readyState]);
-
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Type a message" />
+        <button onClick={() => {
+          sendJsonMessage({ message: text });
+          setText('');
+        }}>Send</button>
       </div>
     </>
   )
